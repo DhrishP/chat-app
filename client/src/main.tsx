87d791c84371e7a4a982eb2ec16628fa7632281d
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2024-05-03 18:27:50 +0530

    added routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import SignUpForm from './components/sign-up.tsx';
+import LoginForm from './components/login.tsx';
+import LogoutForm from './components/logout.tsx';
+import { Toaster } from "@/components/ui/toaster"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path:'/signup',
+    element: <SignUpForm />,
+  },
+  {
+    path:'/login',
+    element: <LoginForm />,
+  },
+  {
+    path:'/logout',
+    element: <LogoutForm />,
+  }
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <Toaster />
+  <RouterProvider router={router} />
+
   </React.StrictMode>,
 )
