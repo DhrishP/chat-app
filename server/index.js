fe9f0ec9aa32e3ab678ef4e9db3df5872a0193c7
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2024-05-05 06:06:07 +0530

    config update for production

--- server/index.js
+++ server/index.js
@@ -6,8 +6,11 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT;
 app.use(cors({ origin: `${process.env.FRONTEND_URL}`, credentials: true }));
@@ -20,8 +23,10 @@ app.use("/api/auth", authRoutes);
 app.use("/message", messageRoutes);
 app.use("/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client","dist", "index.html"));
 });
 
 server.listen(PORT, () => {
